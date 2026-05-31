fb96c4ba468338e03f67e890c2cdc9a472ccab95
Məhəmmədarif Quliyev <mhmmdarifquliyev33@gmail.com>
2026-06-01 00:21:14 +0400

    Word Game

---
+++ JavaScript/nameInput.js
@@ -0,0 +1,9 @@
+let btn = document.querySelector(".btn")
+btn.addEventListener("click", () => {
+    let name1 = document.querySelector(".player1Input").value
+    let name2 = document.querySelector(".player2Input").value
+    localStorage.setItem("player1", name1)
+    localStorage.setItem("player2", name2)
+    
+    window.location.href = "../HTML/startingPage.html"
+})
