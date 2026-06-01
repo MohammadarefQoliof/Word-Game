ce8d22ec65bc264ffe56e1c8e130b58a31f00b7c
Məhəmmədarif Quliyev <mhmmdarifquliyev33@gmail.com>
2026-06-01 22:17:39 +0400

    Word Game

---
+++ JavaScript/category.js
@@ -0,0 +1,49 @@
+let category = document.querySelectorAll(".category")
+let card = document.querySelector(".card")
+let customCategory = document.querySelector(".customCategory")
+let btn = document.querySelector(".btn")
+let categoryValue;
+
+for(let i = 0; i < category.length; i++){
+
+    category[i].addEventListener("click", ()=>{
+        for(let j = 0; j < category.length; j++){
+            category[j].classList.remove("categorySelected")
+        }
+
+        category[i].classList.add("categorySelected")
+
+        categoryValue = category[i].textContent
+        console.log(categoryValue);
+        
+        if(categoryValue == "OTHERS"){
+            card.style.height = "98%"
+            customCategory.style.display = "block"
+            customCategory.style.animation = "appear 0.4s forwards"
+            btn.classList.add("btnNotWork")
+
+            
+            customCategory.addEventListener("input", ()=>{
+                if(customCategory.value != ""){
+                    btn.classList.remove("btnNotWork")
+                }else{
+                    btn.classList.add("btnNotWork")
+                }
+            })
+            btn.addEventListener("click", ()=>{
+                if(customCategory.value != ""){
+                    localStorage.setItem("category", customCategory.value.toUpperCase())
+                    window.location.href = "../HTML/quizWord.html"
+                }
+            })
+        }else{
+            card.style.height = "85%"
+            customCategory.style.animation = "disappear 0.4s forwards"
+            btn.classList.remove("btnNotWork")
+            btn.addEventListener("click", ()=>{
+                localStorage.setItem("category", categoryValue)
+                window.location.href = "../HTML/quizWord.html"
+            })
+        }
+    })
+}
