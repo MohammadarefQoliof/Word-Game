6bb66a80be30a9bda975118eb4c914a88b86ba49
Məhəmmədarif Quliyev <mhmmdarifquliyev33@gmail.com>
2026-06-12 00:40:05 +0400

    Word Game

--- JavaScript/startingPage.js
+++ JavaScript/startingPage.js
@@ -21,8 +21,24 @@ playerNameSpan.innerHTML = name1;
 let btnPlayerName = document.querySelector(".btnPlayerName");
 btnPlayerName.innerHTML = name1;
 
-if(localStorage.getItem("animateDamage")){
-    let damageBg = document.createElement("div") //when the word is found make the damage animation!!!
+if(localStorage.getItem("animate")){
+    let body = document.querySelector("body")
+    let damageBg = document.createElement("div")
+    let damageText = document.createElement("div")
+    let givenDamage = document.createElement("div")
+    let hpText = document.createElement("div")
+
+    damageText.innerHTML = `<div class="damageIcon"></div>Damage`
+    hpText.textContent = "HP"
+    givenDamage.textContent = `-${localStorage.getItem("damage")}`
+
+    givenDamage.classList.add("givenDamage")
+    damageBg.classList.add("damageBg")
+    damageText.classList.add("damageText")
+    hpText.classList.add("hpText")
+
+    damageBg.append(damageText, givenDamage, hpText)
+    body.append(damageBg)
 }
 
 let btn = document.querySelector(".btn");
